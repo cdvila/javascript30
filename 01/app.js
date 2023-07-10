7296c049f92816d1fd26de7347ec6403d9ae6c1b
cdvila <cristiandariovila@gmail.com>
2023-07-10 16:13:03 -0300

    estilizado a mario bros + funcionalidad click

--- 01/app.js
+++ 01/app.js
@@ -1,16 +1,18 @@
 document.addEventListener("keypress", (e) => {
- 
-    if (!document.getElementById("audio" + e.keyCode)) return
-    const playAudio = document.getElementById("audio" + e.keyCode);
-    playAudio.play();
-    playAudio.currentTime = 0
-    const teclaCodigo = "tecla " + e.keyCode;
-    const elemento = document.getElementsByClassName(teclaCodigo);
+  if (!document.getElementById("audio " + e.key)) return;
+  usarTecla(e.key);
+});
 
-    elemento[0].classList.add('active');
-    elemento[0].addEventListener('transitionend', (event) => {
-        elemento[0].classList.remove('active');
-  
-    });
+function usarTecla(tecla) {
+  console.log(tecla);
+  const playAudio = document.getElementById("audio " + tecla);
+  playAudio.play();
+  playAudio.currentTime = 0;
+  const teclaCodigo = "tecla " + tecla;
+  const elemento = document.getElementsByClassName(teclaCodigo);
 
-})
+  elemento[0].classList.add("active");
+  elemento[0].addEventListener("transitionend", (event) => {
+    elemento[0].classList.remove("active");
+  });
+}
