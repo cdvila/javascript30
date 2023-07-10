48bcf82bb0ad2322c259edaa536640b3266508a7
Cristian <cristiandariovila@gmail.com>
2023-07-10 14:51:43 -0300

    drum v2

--- 01/app.js
+++ 01/app.js
@@ -1,15 +1,16 @@
-document.addEventListener("keypress", (e)=>{
-    const tecla = document.getElementById("teclaPresionada");
-    tecla.innerHTML = e.key + " codigo: " + e.keyCode;
-    if(!document.getElementById("audio"+e.keyCode)) return
-        const playAudio = document.getElementById("audio"+e.keyCode);
-        playAudio.play();
-        playAudio.currentTime = 0
-        const teclaCodigo = "tecla " + e.keyCode;
-        const elemento = document.getElementsByClassName(teclaCodigo)     ;
-        console.log(elemento[0]);
-        elemento[0].classList.add('active');
+document.addEventListener("keypress", (e) => {
+ 
+    if (!document.getElementById("audio" + e.keyCode)) return
+    const playAudio = document.getElementById("audio" + e.keyCode);
+    playAudio.play();
+    playAudio.currentTime = 0
+    const teclaCodigo = "tecla " + e.keyCode;
+    const elemento = document.getElementsByClassName(teclaCodigo);
 
+    elemento[0].classList.add('active');
+    elemento[0].addEventListener('transitionend', (event) => {
+        elemento[0].classList.remove('active');
+  
+    });
 
-    console.log(e)
 })
