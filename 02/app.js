c8cbac1f306e92df515ddc116cdbdfb3ce943aba
cdvila <cristiandariovila@gmail.com>
2023-07-11 09:03:49 -0300

    Style | fix seg0

--- 02/app.js
+++ 02/app.js
@@ -1,29 +1,34 @@
-const segundero = document.querySelector('.segundos')
-const minutero = document.querySelector('.minutos')
-const horasMano = document.querySelector('.horas')
+const segundero = document.querySelector(".segundos");
+const minutero = document.querySelector(".minutos");
+const horasMano = document.querySelector(".horas");
 
 function setDate() {
-    const horaActual = new Date();
-    const segundos = horaActual.getSeconds();
-    let gradosSegundos = 0;
-    if (segundos > 0) {
-        gradosSegundos = segundos * 6;
-    }
-    segundero.style.transform = `rotate(${gradosSegundos + 90}deg)`
-    console.log(segundos)
-    console.log(gradosSegundos+90)
+  const horaActual = new Date();
+  const segundos = horaActual.getSeconds();
+  let gradosSegundos = (segundos / 60) * 360 + 90;
+  if (segundos == 0) {
+    segundero.style.transition = "all 0.0s";
+    segundero.style.transform = `rotate(90deg)`;
+  }
+  segundero.style.transform = `rotate(${gradosSegundos}deg)`;
 
-    const minutos = horaActual.getMinutes();
-    const gradosMinutos = minutos * 6;
-    minutero.style.transform = `rotate(${gradosMinutos + 90}deg)`
+  const minutos = horaActual.getMinutes();
+  if (minutos == 0) {
+    minutero.style.transition = "all 0.0s";
+    minutero.style.transform = `rotate(90deg)`;
+  }
+  const gradosMinutos = minutos * 6;
+  minutero.style.transform = `rotate(${gradosMinutos + 90}deg)`;
 
-    const horas = horaActual.getHours();
-    const gradosHoras = horas * 30;
-    horasMano.style.transform = `rotate(${gradosHoras + 90}deg)`
-
-
+  const horas = horaActual.getHours();
+  if (horas == 0) {
+    horasMano.style.transition = "all 0.0s";
+    horasMano.style.transform = `rotate(90deg)`;
+  }
+  const gradosHoras = horas * 30;
+  horasMano.style.transform = `rotate(${gradosHoras + 90}deg)`;
 }
 
 setInterval(() => {
-    setDate()
-}, 1000); 
+  setDate();
+}, 1000);
